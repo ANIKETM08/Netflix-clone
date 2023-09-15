18dc90159f3dccf84b2ac78c45fd6ff0277fb1b8
Aniket Malviya <112813591+ANIKETM08@users.noreply.github.com>
2023-09-15 12:46:06 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,7 +21,8 @@ function Navbar() {
 
   return (
     <div className={`navbar ${show && "nav__black"}`}>
-      <h1 style={{color: "Blue", fontSize: "22px"}}>MOVIEINFO</h1>
+      <h1 style={{background: "-webkit-linear-gradient(#eee, #333)",
+  webkitBackgroundClip: "text", webkitTextFillColor: "transparent", fontSize: "25px"}}>MOVIEINFO</h1>
       <img
         className="nav__avatar"
         src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
